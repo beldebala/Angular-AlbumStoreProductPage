ce175caf2934c4fcf0745a26d32627bc396f6fcd
bbelde.linkedin@gmail.com <bbelde.linkedin@gmail.com>
2019-08-27 22:58:30 +0530

    Completed the module

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+  private _ablbumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number){
+       return this._http.get(this._ablbumUrl).map((response) => response.json());
+  }
+
+}
